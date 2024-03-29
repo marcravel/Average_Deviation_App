23d8ab1db9df82349b87dcec71af1e0c4a8afdd2
Marc Ravelontsalama <marc.ravelontsalama@gmail.com>
2024-03-29 23:12:06 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Average_Deviation_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.valuesListBox = new System.Windows.Forms.ListBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.Operations = new System.Windows.Forms.GroupBox();
+            this.averageRadio = new System.Windows.Forms.RadioButton();
+            this.deviationRadio = new System.Windows.Forms.RadioButton();
+            this.deviationAvgRadio = new System.Windows.Forms.RadioButton();
+            this.resultRichText = new System.Windows.Forms.RichTextBox();
+            this.standartDeviationRadio = new System.Windows.Forms.RadioButton();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.Operations.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Location = new System.Drawing.Point(12, 12);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(245, 27);
+            this.valueTextBox.TabIndex = 0;
+            this.valueTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.valueTextBox_KeyPress);
+            // 
+            // valuesListBox
+            // 
+            this.valuesListBox.FormattingEnabled = true;
+            this.valuesListBox.ItemHeight = 20;
+            this.valuesListBox.Location = new System.Drawing.Point(12, 45);
+            this.valuesListBox.Name = "valuesListBox";
+            this.valuesListBox.Size = new System.Drawing.Size(245, 344);
+            this.valuesListBox.TabIndex = 1;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(285, 60);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(186, 42);
+            this.calculateBtn.TabIndex = 3;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // Operations
+            // 
+            this.Operations.Controls.Add(this.standartDeviationRadio);
+            this.Operations.Controls.Add(this.deviationAvgRadio);
+            this.Operations.Controls.Add(this.deviationRadio);
+            this.Operations.Controls.Add(this.averageRadio);
+            this.Operations.Location = new System.Drawing.Point(285, 108);
+            this.Operations.Name = "Operations";
+            this.Operations.Size = new System.Drawing.Size(186, 147);
+            this.Operations.TabIndex = 4;
+            this.Operations.TabStop = false;
+            this.Operations.Text = "Operations";
+            // 
+            // averageRadio
+            // 
+            this.averageRadio.AutoSize = true;
+            this.averageRadio.Checked = true;
+            this.averageRadio.Location = new System.Drawing.Point(6, 26);
+            this.averageRadio.Name = "averageRadio";
+            this.averageRadio.Size = new System.Drawing.Size(91, 24);
+            this.averageRadio.TabIndex = 0;
+            this.averageRadio.TabStop = true;
+            this.averageRadio.Text = "Avarage";
+            this.averageRadio.UseVisualStyleBackColor = true;
+            // 
+            // deviationRadio
+            // 
+            this.deviationRadio.AutoSize = true;
+            this.deviationRadio.Location = new System.Drawing.Point(6, 56);
+            this.deviationRadio.Name = "deviationRadio";
+            this.deviationRadio.Size = new System.Drawing.Size(100, 24);
+            this.deviationRadio.TabIndex = 1;
+            this.deviationRadio.Text = "Deviation";
+            this.deviationRadio.UseVisualStyleBackColor = true;
+            // 
+            // deviationAvgRadio
+            // 
+            this.deviationAvgRadio.AutoSize = true;
+            this.deviationAvgRadio.Location = new System.Drawing.Point(6, 86);
+            this.deviationAvgRadio.Name = "deviationAvgRadio";
+            this.deviationAvgRadio.Size = new System.Drawing.Size(133, 24);
+            this.deviationAvgRadio.TabIndex = 2;
+            this.deviationAvgRadio.Text = "Deviation Avg";
+            this.deviationAvgRadio.UseVisualStyleBackColor = true;
+            // 
+            // resultRichText
+            // 
+            this.resultRichText.Location = new System.Drawing.Point(285, 261);
+            this.resultRichText.Name = "resultRichText";
+            this.resultRichText.Size = new System.Drawing.Size(186, 188);
+            this.resultRichText.TabIndex = 5;
+            this.resultRichText.Text = "";
+            // 
+            // standartDeviationRadio
+            // 
+            this.standartDeviationRadio.AutoSize = true;
+            this.standartDeviationRadio.Location = new System.Drawing.Point(6, 116);
+            this.standartDeviationRadio.Name = "standartDeviationRadio";
+            this.standartDeviationRadio.Size = new System.Drawing.Size(168, 24);
+            this.standartDeviationRadio.TabIndex = 3;
+            this.standartDeviationRadio.Text = "Standart Deviation";
+            this.standartDeviationRadio.UseVisualStyleBackColor = true;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(12, 395);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(245, 54);
+            this.clearBtn.TabIndex = 6;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(285, 12);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(186, 42);
+            this.addBtn.TabIndex = 7;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 465);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.resultRichText);
+            this.Controls.Add(this.Operations);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.valuesListBox);
+            this.Controls.Add(this.valueTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Operations.ResumeLayout(false);
+            this.Operations.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.ListBox valuesListBox;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.GroupBox Operations;
+        private System.Windows.Forms.RadioButton deviationAvgRadio;
+        private System.Windows.Forms.RadioButton deviationRadio;
+        private System.Windows.Forms.RadioButton averageRadio;
+        private System.Windows.Forms.RichTextBox resultRichText;
+        private System.Windows.Forms.RadioButton standartDeviationRadio;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button addBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Average_Deviation_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            valuesListBox.Items.Clear();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if (valueTextBox.Text != string.Empty)
+            {
+                valuesListBox.Items.Add(Convert.ToDecimal(valueTextBox.Text));
+            }
+            else
+            {
+                valueTextBox.Focus();
+            }
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            decimal avg = 0;
+            if (valuesListBox.Items.Count > 0)
+            {
+                foreach (var item in valuesListBox.Items)
+                {
+                    avg += Decimal.Parse(item.ToString());
+                }
+                avg /= valuesListBox.Items.Count;
+            }
+
+            if (averageRadio.Checked)
+            {               
+                resultRichText.Text = $"{avg}";
+            }
+            else if (deviationRadio.Checked)
+            {
+                if (valuesListBox.SelectedIndex < 0)
+                {
+                    valueTextBox.Text = "Select a value";
+                }
+                else
+                {
+                    decimal dev = Decimal.Parse(valuesListBox.Items[valuesListBox.SelectedIndex].ToString()) - avg;
+                    resultRichText.Text = $"Deviation of {valuesListBox.Items[valuesListBox.SelectedIndex].ToString()} " +
+                        $": {dev}";
+                }
+
+            }
+            else if (deviationAvgRadio.Checked)
+            {
+
+            }
+            else if (standartDeviationRadio.Checked)
+            {
+
+            }
+
+        }
+
+        private void valueTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(valueTextBox.Text) && e.KeyChar == (char)Keys.Return)
+            { 
+                valuesListBox.Items.Add(valueTextBox.Text);
+                valueTextBox.Text = string.Empty;
+            }
+            else if (valuesListBox.Items.Count > 0 && e.KeyChar == 'c')
+            {
+                calculateBtn.PerformClick();
+            }
+        }
+    }
+}
